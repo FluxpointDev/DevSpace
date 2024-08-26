a1ca48c9ccab9b9c8860b1b0f53bcc2a86179ea2
Builderb <builderbgamer@gmail.com>
2024-08-26 19:59:50 +0100

    More setup changes and added smtp validation

---
+++ DevSpaceWeb/Models/SmtpModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpaceWeb.Models;
+
+public class SmtpModel
+{
+    [Required(ErrorMessage = "Smtp host is required")]
+    [MaxLength(100, ErrorMessage = "Smtp host has a maximum of 100 characters")]
+    public string Host { get; set; }
+    public int Port { get; set; }
+
+    [Required(ErrorMessage = "Smtp email address is required")]
+    [MaxLength(100, ErrorMessage = "Smtp email address has a maximum of 100 characters")]
+    [EmailAddress(ErrorMessage = "Email is invalid")]
+    public string EmailAddress { get; set; }
+
+    [Required(ErrorMessage = "Smtp user is required")]
+    [MaxLength(100, ErrorMessage = "Smtp user has a maximum of 100 characters")]
+    public string User { get; set; }
+
+    [Required(ErrorMessage = "Smtp password is required")]
+    [MaxLength(100, ErrorMessage = "Smtp password has a maximum of 100 characters")]
+    public string Password { get; set; }
+}
