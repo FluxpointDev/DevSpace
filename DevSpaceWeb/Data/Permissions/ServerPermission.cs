52776ba76bd27b5a1403b3f9f541c11048e9574b
Builderb <builderbgamer@gmail.com>
2024-10-27 15:57:09 +0000

    Adding permissions

--- DevSpaceWeb/Data/Permissions/ServerPermission.cs
+++ DevSpaceWeb/Data/Permissions/ServerPermission.cs
@@ -6,4 +6,7 @@ public enum ServerPermission : ulong
     ViewServer = 1L << 0,
     ManagePermissions = 1L << 1,
     ServerAdministrator = 1L << 2,
+    DockerController = 1L << 3,
+    DockerManager = 1L << 4,
+    DockerAdministrator = 1L << 5,
 }
