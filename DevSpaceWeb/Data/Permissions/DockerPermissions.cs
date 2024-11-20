979fc0745722ca6b2e5ebac4758980cb130d10ae
Builderb <builderbgamer@gmail.com>
2024-11-20 18:57:22 +0000

    Add docker manager permissions

--- DevSpaceWeb/Data/Permissions/DockerPermissions.cs
+++ DevSpaceWeb/Data/Permissions/DockerPermissions.cs
@@ -16,6 +16,35 @@ public class DockerPermissions
         if (permission != DockerPermission.DockerAdministrator && Has(DockerPermission.DockerAdministrator))
             return true;
 
+        if (permission != DockerPermission.DockerManager && Has(DockerPermission.DockerManager))
+        {
+            // Manager permissions
+            switch (permission)
+            {
+                case DockerPermission.ContainerInspect:
+                case DockerPermission.ContainerLogs:
+                case DockerPermission.ContainerStats:
+                case DockerPermission.ControlContainers:
+                case DockerPermission.ManageContainers:
+                case DockerPermission.ManageCustomTemplates:
+                case DockerPermission.ManageImages:
+                case DockerPermission.ManageNetworks:
+                case DockerPermission.ManageRegistries:
+                case DockerPermission.ManageSettings:
+                case DockerPermission.ManageVolumes:
+                case DockerPermission.ViewContainers:
+                case DockerPermission.ViewCustomTemplates:
+                case DockerPermission.ViewEvents:
+                case DockerPermission.ViewImages:
+                case DockerPermission.ViewNetworks:
+                case DockerPermission.ViewPlugins:
+                case DockerPermission.ViewRegistries:
+                case DockerPermission.ViewTemplates:
+                case DockerPermission.ViewVolumes:
+                    return true;
+            }
+        }
+
         ulong Flag = (ulong)permission;
         return (Raw & Flag) == Flag;
     }
