cb77855265787590cbb61f69b5c57f1dddd3a9a1
Builderb <builderbgamer@gmail.com>
2024-11-20 18:49:24 +0000

    Add docker permission checks

--- DevSpaceWeb/Data/Permissions/DockerPermissions.cs
+++ DevSpaceWeb/Data/Permissions/DockerPermissions.cs
@@ -13,6 +13,10 @@ public class DockerPermissions
 
     public bool Has(DockerPermission permission)
     {
+        ulong AdminFlag = (ulong)DockerPermission.DockerAdministrator;
+        if ((Raw & AdminFlag) == AdminFlag)
+            return true;
+
         ulong Flag = (ulong)permission;
         return (Raw & Flag) == Flag;
     }
