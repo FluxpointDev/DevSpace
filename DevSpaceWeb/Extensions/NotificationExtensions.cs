a0ceb7bbcd2185c7a3b8742a7d3b117ceba0c104
Builderb <builderbgamer@gmail.com>
2024-12-05 02:19:12 +0000

    Added permission warning messages and new Assign Roles permission.

---
+++ DevSpaceWeb/Extensions/NotificationExtensions.cs
@@ -0,0 +1,58 @@
+﻿using DevSpaceWeb.Data.Permissions;
+using Radzen;
+
+namespace DevSpaceWeb;
+
+public static class NotificationExtensions
+{
+    public static void ShowPermissionWarning(this NotificationService notify, TeamPermission permission)
+        => ShowPermissionWarningInternal(notify, permission.GetName());
+
+    public static void ShowPermissionWarningInternal(NotificationService notify, string permission)
+    {
+        notify.Notify(new NotificationMessage
+        {
+            Severity = NotificationSeverity.Warning,
+            Duration = 40000,
+            Summary = "Permission Error",
+            Detail = $"You do not have {permission} permission."
+           
+        });
+    }
+
+
+
+    public static void ShowErrorWarning(this NotificationService notify, NotificationErrorType error)
+    {
+        string Message = "";
+        switch (error)
+        {
+            case NotificationErrorType.AccountLoadFailed:
+                Message = "Failed to get account.";
+                break;
+            case NotificationErrorType.PreviewNotEnabled:
+                Message = "Preview mode is not enabled.";
+                break;
+            case NotificationErrorType.AuthenticatorNoDevices:
+                Message = "You do not have any authenticator devices linked to your account.";
+                break;
+            case NotificationErrorType.PasskeysNoDevices:
+                Message = "You do not have any passkey devices linked to your account.";
+                break;
+        }
+        notify.Notify(new NotificationMessage
+        {
+            Severity = NotificationSeverity.Warning,
+            Duration = 40000,
+            Summary = "Error",
+            Detail = Message
+
+        });
+    }
+
+
+}
+public enum NotificationErrorType
+{
+    AccountLoadFailed, PreviewNotEnabled, AuthenticatorNoDevices, PasskeysNoDevices
+}
---
+++ DevSpaceWeb/Extensions/PermissionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using DevSpaceWeb.Data.Permissions;
+
+namespace DevSpaceWeb;
+
+public static class PermissionExtensions
+{
+    public static string GetName(this TeamPermission permission)
+    {
+        switch (permission)
+        {
+            case TeamPermission.GlobalAdministrator:
+                return "Global Administrator";
+            case TeamPermission.ManageMembers:
+                return "Manage Members";
+            case TeamPermission.AssignRoles:
+                return "Assign Roles";
+            case TeamPermission.ManagePermissions:
+                return "Manage Permissions";
+            case TeamPermission.ManageResources:
+                return "Manage Resources";
+            case TeamPermission.ManageRoles:
+                return "Manage Roles";
+            case TeamPermission.ManageTeam:
+                return "Manage Team";
+            case TeamPermission.TeamAdministrator:
+                return "Team Administrator";
+            case TeamPermission.ViewMembers:
+                return "View Members";
+            case TeamPermission.ViewPermissions:
+                return "View Permissions";
+        }
+        return "Error!!";
+
+    }
+}
