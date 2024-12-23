e29bbdbee97323e6ac88adc0e2e46fb2f9c0f917
Builderb <builderbgamer@gmail.com>
2024-12-23 23:06:12 +0000

    Disable external login and disable buttons

--- DevSpaceWeb/Extensions/Utils.cs
+++ DevSpaceWeb/Extensions/Utils.cs
@@ -11,7 +11,7 @@ namespace DevSpaceWeb;
 
 public static class Utils
 {
-    public static string? GetBrowserName(SessionBrowserType type)
+    public static string GetBrowserName(SessionBrowserType type)
     {
         switch (type)
         {
