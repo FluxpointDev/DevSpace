969f8b6a199d1aa03f2d436f9bce4cfb77a8adfd
Builderb <builderbgamer@gmail.com>
2024-12-31 19:49:47 +0000

    First development of API clients

--- DevSpaceWeb/Data/API/APIClient.cs
+++ DevSpaceWeb/Data/API/APIClient.cs
@@ -1,4 +1,5 @@
 ﻿using DevSpaceWeb.Data.Permissions;
+using DevSpaceWeb.Data.Teams;
 using MongoDB.Bson;
 
 namespace DevSpaceWeb.Data.API;
@@ -6,11 +7,27 @@ namespace DevSpaceWeb.Data.API;
 public class APIClient
 {
     public ObjectId Id { get; set; }
+    public string Name { get; set; }
     public ObjectId OwnerId { get; set; }
     public ObjectId AccessUserId { get; set; }
     public ObjectId? TeamId { get; set; }
     public bool IsDisabled { get; set; }
+    public bool IsInstanceAdmin { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public bool UseCustomPermissions { get; set; }
     public PermissionsSet CustomPermissions { get; set; } = new PermissionsSet();
+
+    public bool HasAccess(TeamMemberData member)
+    {
+        if (OwnerId == member.UserId || member.HasTeamPermission(TeamPermission.ManageAllAPIs))
+            return true;
+        return false;
+    }
+
+    public bool CanManage(TeamMemberData member)
+    {
+        if ((member.HasTeamPermission(TeamPermission.ManageOwnAPIs) && OwnerId == member.UserId) || member.HasTeamPermission(TeamPermission.ManageAllAPIs))
+            return true;
+        return false;
+    }
 }
---
+++ DevSpaceWeb/Models/Teams/TeamCreateAPIModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpaceWeb.Models.Teams;
+
+public class TeamCreateAPIModel
+{
+    [Required(ErrorMessage = "API client name is required")]
+    [MinLength(3, ErrorMessage = "API client name requires minimum of 3 characters")]
+    [MaxLength(32, ErrorMessage = "API client name has a maximum of 32 characters")]
+    public string Name { get; set; }
+}
