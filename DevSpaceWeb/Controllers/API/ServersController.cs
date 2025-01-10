bfe73cee7ede6d8f6ae893de9c73c7d4902c0bd8
Builderb <builderbgamer@gmail.com>
2025-01-10 00:30:21 +0000

    Add list servers route

--- DevSpaceWeb/Controllers/API/ServersController.cs
+++ DevSpaceWeb/Controllers/API/ServersController.cs
@@ -1,5 +1,6 @@
 ﻿using DevSpaceWeb.API;
 using DevSpaceWeb.API.Servers;
+using DevSpaceWeb.API.Teams;
 using DevSpaceWeb.API.Users;
 using DevSpaceWeb.Data.Permissions;
 using DevSpaceWeb.Database;
@@ -31,5 +32,16 @@ public class ServersController : APIController
             return Forbidden("Client does not have View Server permission.");
 
         return Ok(new ServerJson(server));
+    }
+
+    [HttpGet("/api/teams/{teamId?}/servers")]
+    [SwaggerOperation("Get a list of servers.", "")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Success", typeof(ResponseData<ServerJson[]>))]
+    public async Task<IActionResult> GetServers([FromRoute] string teamId = "")
+    {
+        if (string.IsNullOrEmpty(teamId) || !ObjectId.TryParse(teamId, out ObjectId obj) || !_DB.Teams.Cache.TryGetValue(obj, out Data.Teams.TeamData? Team))
+            return BadRequest("Could not find team.");
+
+        return Ok(_DB.Servers.Cache.Values.Where(x => (Client.IsInstanceAdmin || x.TeamId == Client.TeamId.GetValueOrDefault()) && Client.HasServerPermission(x, ServerPermission.ViewServer)).Select(x => new ServerJson(x)));
     }
 }
