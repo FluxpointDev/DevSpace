e06ec282e61458e211ab486ab82d6b62a7b3e0f4
Builderb <builderbgamer@gmail.com>
2025-01-31 22:33:48 +0000

    Add testing feature for links

---
+++ DevSpaceWeb/Data/Links/LinkData.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+
+namespace DevSpaceWeb.Data.Links;
+
+public class LinkData
+{
+    public static LinkData Static = new LinkData();
+
+    public string Title = "Hello World!";
+    public string Description = "Test description here";
+    public string BackgroundUrl;
+    public bool RequireAuthentication;
+    public bool RequireMember;
+    public bool RequireRoles;
+    public HashSet<ObjectId> RestrictedRoles = new HashSet<ObjectId>();
+    public int ButtonTimer = 3;
+    public bool RequireCode;
+    public string? Code;
+    public string? Link;
+    public bool IsDownload;
+
+    public string GetDomain()
+    {
+        return Link.Replace("https://", "").Replace("http://", "").Split('/').First();
+    }
+}
--- DevSpaceWeb/Pages/LinkPage.cshtml.cs
+++ DevSpaceWeb/Pages/LinkPage.cshtml.cs
@@ -1,11 +1,60 @@
+using DevSpaceWeb.Data.Links;
+using DevSpaceWeb.Data.Teams;
+using DevSpaceWeb.Data.Users;
+using DevSpaceWeb.Database;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DevSpaceWeb.Pages
 {
     public class LinkPageModel : PageModel
     {
-        public void OnGet()
+        public LinkPageModel(UserManager<AuthUser> userManager)
         {
+            UserManager = userManager;
+        }
+
+        public UserManager<AuthUser> UserManager;
+
+        public bool RequireAuth;
+        public bool RequireMember;
+        public bool RequireRoles;
+
+        public bool IsAuthenticated;
+        public bool IsMember;
+        public bool HasRoles;
+
+        public async Task OnGet()
+        {
+            if (LinkData.Static.RequireAuthentication)
+            {
+                RequireAuth = true;
+                AuthUser? User = null;
+
+                if (Request.HttpContext.User.Identity != null && Request.HttpContext.User.Identity.IsAuthenticated)
+                    User = await UserManager.GetUserAsync(Request.HttpContext.User);
+
+                if (User != null && User.Disabled == null)
+                    IsAuthenticated = true;
+
+                if (LinkData.Static.RequireMember)
+                {
+                    RequireMember = true;
+
+                    TeamMemberData? Member = _DB.Teams.Cache.Values.First().GetMember(User);
+                    if (Member != null)
+                        IsMember = true;
+
+                    if (LinkData.Static.RequireRoles)
+                    {
+                        RequireRoles = true;
+                        if (Member != null && Member.Roles.Any())
+                            HasRoles = true;
+
+                    }
+                }
+            }
+
         }
     }
 }
