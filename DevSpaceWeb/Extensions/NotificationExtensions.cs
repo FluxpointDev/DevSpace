def699ee9018877de62d2c1516edd162455acf79
Builderb <builderbgamer@gmail.com>
2025-04-08 22:32:17 +0100

    Add remove avatar button under profile avatar icon instead of the change avatar dialog

--- DevSpaceWeb/Extensions/NotificationExtensions.cs
+++ DevSpaceWeb/Extensions/NotificationExtensions.cs
@@ -41,6 +41,9 @@ public static class NotificationExtensions
             case NotificationErrorType.PasskeysNoDevices:
                 Message = "You do not have any passkey devices linked to your account.";
                 break;
+            case NotificationErrorType.UploadNoFile:
+                Message = "You need to add a file to upload.";
+                break;
         }
         notify.Notify(new NotificationMessage
         {
@@ -56,5 +59,5 @@ public static class NotificationExtensions
 }
 public enum NotificationErrorType
 {
-    AccountLoadFailed, PreviewNotEnabled, AuthenticatorNoDevices, PasskeysNoDevices
+    AccountLoadFailed, PreviewNotEnabled, AuthenticatorNoDevices, PasskeysNoDevices, UploadNoFile
 }
