d3660e4f19398577bcdf127859cae0d6dcab29c0
Builderb <builderbgamer@gmail.com>
2025-05-04 22:19:49 +0100

    Add color property to team member API response

--- DevSpaceWeb/API/Teams/MemberJson.cs
+++ DevSpaceWeb/API/Teams/MemberJson.cs
@@ -11,6 +11,7 @@ public class MemberJson
         user_id = data.UserId.ToString();
         joined_at = data.JoinedAt;
         is_disabled = data.Disabled != null;
+        color = data.GetCurrentColor();
         roles = data.GetCachedRoles().Select(x => new RoleJson(x, viewPerms)).ToArray();
         nickname = data.NickName;
         if (viewPerms)
@@ -21,6 +22,7 @@ public class MemberJson
     public string user_id;
     public bool is_disabled;
     public RoleJson[] roles;
+    public string color;
     public string? nickname;
     public PermissionsSet? permissions;
     public DateTime joined_at;
