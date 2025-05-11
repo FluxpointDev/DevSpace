9fa4e7ad8139bb6cf3af45669ba16ddf8a08dd4b
Builderb <builderbgamer@gmail.com>
2025-05-12 00:24:18 +0100

    Move system network check to shared extension

--- DevSpaceShared/Extensions/DockerExtensions.cs
+++ DevSpaceShared/Extensions/DockerExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Docker.DotNet.Models;
+﻿using DevSpaceShared.Data;
+using Docker.DotNet.Models;
 
 namespace DevSpaceShared;
 public static class DockerExtensions
@@ -11,6 +12,29 @@ public static class DockerExtensions
             case "restarting":
             case "paused":
             case "healthy":
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool IsNetworkSystem(this DockerContainerNetwork network)
+    {
+        return IsNetworkSystem(network.Name);
+    }
+
+    public static bool IsNetworkSystem(this DockerNetworkInfo network)
+    {
+        return IsNetworkSystem(network.Name);
+    }
+
+    public static bool IsNetworkSystem(string? name)
+    {
+        switch (name)
+        {
+            case "host":
+            case "bridge":
+            case "none":
                 return true;
         }
 
