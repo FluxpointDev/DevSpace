f806e5baee9645928460a6afde1379c6b58fb85a
Misha133 <61027276+Misha-133@users.noreply.github.com>
2025-05-25 01:42:58 +0300

    annoying aaaaa

--- DevSpaceWeb/Program.cs
+++ DevSpaceWeb/Program.cs
@@ -78,8 +78,6 @@ public class Program
     public static async Task Main(string[] args)
     {
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-        builder.Configuration
-            .AddJsonFile("appsettings.Production.json");
 
         ShowDemoLink = builder.Configuration.GetValue<bool>("ShowDemoLink");
         IsDevMode = Environment.GetEnvironmentVariable("DEVSPACE") == "Development";
