75a0acc825e83d887637103bcaaf2d37a923b2a8
Builderb <builderbgamer@gmail.com>
2025-05-24 22:58:48 +0100

    Add per-page items count selector to data grids for 15, 25, 50 and 100

--- DevSpaceWeb/Extensions/Static.cs
+++ DevSpaceWeb/Extensions/Static.cs
@@ -2,6 +2,7 @@
 
 public static class Static
 {
+    public static IEnumerable<int> PageSizeOptions = new int[] { 15, 25, 50, 100 };
     public static string DefaultRoleColor = "#c4c9ce";
     public static string DefaultBackgroundUrl = "https://cdn.fluxpoint.dev/devspace/background.webp";
 }
