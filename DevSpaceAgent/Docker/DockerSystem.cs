b1f7fa93f314a8998759e0560137f5066bf400b1
Builderb <builderbgamer@gmail.com>
2025-05-30 22:37:30 +0100

    Testing drives json

--- DevSpaceAgent/Docker/DockerSystem.cs
+++ DevSpaceAgent/Docker/DockerSystem.cs
@@ -6,44 +6,13 @@ namespace DevSpaceAgent.Docker;
 
 public static class DockerSystem
 {
-    private static readonly string[] SizeSuffixes =
-               ["bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-    public static string SizeSuffix(Int64 value, int decimalPlaces = 1)
-    {
-        if (decimalPlaces < 0) { throw new ArgumentOutOfRangeException("decimalPlaces"); }
-        if (value < 0) { return "-" + SizeSuffix(-value, decimalPlaces); }
-        if (value == 0) { return string.Format("{0:n" + decimalPlaces + "} bytes", 0); }
-
-        // mag is 0 for bytes, 1 for KB, 2, for MB, etc.
-        int mag = (int)Math.Log(value, 1024);
-
-        // 1L << (mag * 10) == 2 ^ (10 * mag) 
-        // [i.e. the number of bytes in the unit corresponding to mag]
-        decimal adjustedSize = (decimal)value / (1L << (mag * 10));
-
-        // make adjustment when the value is large enough that
-        // it would round up to 1000 or more
-        if (Math.Round(adjustedSize, decimalPlaces) >= 1000)
-        {
-            mag += 1;
-            adjustedSize /= 1024;
-        }
-        decimalPlaces = 0;
-        if (mag > 2)
-            decimalPlaces = 2;
-
-        return string.Format("{0:n" + decimalPlaces + "} {1}",
-            adjustedSize,
-            SizeSuffixes[mag]);
-    }
-
     public static async Task<SystemInfoFullResponse> GetHostInfoAsync(DockerClient client)
     {
         global::Docker.DotNet.Models.SystemInfoResponse Info = await client.System.GetSystemInfoAsync();
         VersionResponse Version = await client.System.GetVersionAsync();
         DriveInfo[] Drives = DriveInfo.GetDrives();
         DriveInfo CurrentDrive = Drives.First();
-
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(Drives, Newtonsoft.Json.Formatting.Indented));
         return new SystemInfoFullResponse
         {
             Docker = new SystemDockerInfo
