7649a637ad1f56971a676fc2ff52c7cee60094c3
Builderb <builderbgamer@gmail.com>
2025-06-21 14:55:10 +0100

    Add per-items pagination option 75

--- DevSpaceWeb/Extensions/Static.cs
+++ DevSpaceWeb/Extensions/Static.cs
@@ -2,8 +2,8 @@
 
 public static class Static
 {
-    public static IEnumerable<int> ListPageSizeOptions = new int[] { 5, 10, 15, 20 };
-    public static IEnumerable<int> TablePageSizeOptions = new int[] { 15, 25, 50, 100 };
+    public static IEnumerable<int> ListPageSizeOptions = new int[] { 5, 10, 15, 20, 25 };
+    public static IEnumerable<int> TablePageSizeOptions = new int[] { 15, 25, 50, 75, 100 };
     public static string DefaultRoleColor = "#c4c9ce";
     public static string DefaultBackgroundUrl = "https://cdn.fluxpoint.dev/devspace/background.webp";
 }
