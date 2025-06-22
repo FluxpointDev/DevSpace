d3068f0ffce864002294c5eb555eaad4d9dcb8d3
Builderb <builderbgamer@gmail.com>
2025-06-22 20:56:53 +0100

    Fixed pagination issues for servers/consoles list

---
+++ DevSpaceWeb/Components/Custom/RadzenDataListFixed.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+
+namespace DevSpaceWeb.Components.Custom;
+
+public partial class RadzenDataListFixed<TItem> : RadzenDataList<TItem>
+{
+    protected override async Task OnPageSizeChanged(int value)
+    {
+        await base.OnPageSizeChanged(value);
+        await PageSizeChanged.InvokeAsync(value);
+    }
+
+    [Parameter]
+    public EventCallback<int> PageSizeChanged { get; set; }
+}
