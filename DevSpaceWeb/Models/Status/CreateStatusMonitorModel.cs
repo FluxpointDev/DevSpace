e54c421def40feff40258f8cb171ed2327c4121b
Builderb <builderbgamer@gmail.com>
2025-08-02 01:32:12 +0100

    Add more status stuff

---
+++ DevSpaceWeb/Models/Status/CreateStatusMonitorModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpaceWeb.Models.Status;
+
+public class CreateStatusMonitorModel
+{
+    [Required(ErrorMessage = "Monitor name is required")]
+    public string? Name { get; set; }
+}
---
+++ DevSpaceWeb/Models/Status/CreateStatusPageModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpaceWeb.Models.Status;
+
+public class CreateStatusPageModel
+{
+    [Required(ErrorMessage = "Page name is required")]
+    public string? Name { get; set; }
+}
